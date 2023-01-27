afb9b5303c3c49a5353b3c40dd693bcddd4725ef
rgo555 <95858479+rgo555@users.noreply.github.com>
2023-01-27 14:00:15 +0100

    27/01

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -48,22 +48,51 @@ public class AI : MonoBehaviour
             break;
         }
     }
+     
+    void Travle ()
+    {
+        if(agent.remainingDistance <= 0.2)
+        {
+            currentState = State.Patrolling;
+        }
+
+        if8Vector.Distance((transform.position, player.position) < visionRange)
+    }
 
     void Patrol()
     {
         agent.destination = destinationPoints[destinationIndex].position;
 
-        if(Vector3.Distance(transform.position, destinationPoints[destinationIndex].position) < 1)
+        /*if(Vector3.Distance(transform.position, destinationPoints[destinationIndex].position) < 1)
         {
             destinationIndex = Random.Range(0, destinationPoints.Length);
+        }*/
+         
+        if(RandomPoint(transform.position, patrolRange, out randomPosition))
+        {
+            agent.destination = randomPosition;
         }
 
         if(Vector3.Distance(transform.position, player.position) < visionRange)
         {
             currentState = State.Chasing; 
+        
+        bool RandomPoint (Vector3 cemter, float range, out Vector3 private void OnParticleCollision) 
+        {
+            Vector3 randompoint = center + Random.insideUnitSohere + range;
+            NavMeshhit hit; 
+            if(NavMesh.SamplePosition(randomPoint, out hit,, 4, NavMesh.AllAreas))
+            {
+                point = hit.positionreturn true;
+            }
+            point = Vector3.Zero;
+            return false;
         }
+
+        currentState = State.Traveling;
     }
 
+    
     void Chase()
     {
         agent.destination = player.position; 
